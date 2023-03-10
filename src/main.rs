b13409071ce87188da63d23b24290dac6c34cd81
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-03-10 16:39:22 +0800

    updated

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,21 @@ struct Items<T> {
 }
 
 fn print_tracts(tracks: vec<&Track>) {
-
+    for track in tracks {
+        println!("{}", track.name);
+        println!("{}", track.album.name);
+        println!(
+            "{}",
+            track
+                .album
+                .artists
+                .iter()
+                .map(|artist| artist.name.to_string())
+                .collect::<String>()
+        );
+        println!("{}", track.external_urls.spotify);
+        println!("------------------------")
+    }
 }
 
 #[tokio::main]
